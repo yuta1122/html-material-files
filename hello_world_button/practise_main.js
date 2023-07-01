222e005aa5607be4bcf101722c8aa18b2a889cbc
yuta1122 <94837546+yuta1122@users.noreply.github.com>
2023-07-01 12:04:22 +0900

    hello_world_execを追加

---
+++ hello_world_button/practise_main.js
@@ -0,0 +1,9 @@
+function hello( ) {
+    var log = document.getElementById("output");
+    log.innerHTML = "こんにちは。<br>";
+}
+
+function reset( ) {
+    var log = document.getElementById("output");
+    log.innerHTML = "";
+}
